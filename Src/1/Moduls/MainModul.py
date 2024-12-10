fc5832b239d0bc2aa0c9a4d143baaed7babdba36
Varezhka69 <vareckakuznecova@gmail.com>
2024-12-10 22:03:59 +0300

    Update MainModul.py

--- Src/1/Moduls/MainModul.py
+++ Src/1/Moduls/MainModul.py
@@ -1,1 +1,21 @@
+from module_image_loader import ImageLoader
+from module_image_info import ImageInfo
+from module_image_renamer import ImageRenamer
 
+def main():
+    image_path = 'path/to/your/image.jpg'  # Укажите путь к вашему изображению
+    image_loader = ImageLoader(image_path)
+
+    # Получаем информацию об изображении
+    image_info = ImageInfo(image_loader)
+    info = image_info.get_image_info()
+    print(info)
+
+    # Переименовываем изображение
+    new_name = 'new_image_name.jpg'
+    renamer = ImageRenamer(image_loader)
+    renamer.rename_image(new_name)
+    print(f'Изображение переименовано в {new_name}')
+
+if __name__ == "__main__":
+    main()

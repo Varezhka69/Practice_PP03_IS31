6a216860bd80a1211f4d5bba31ddf6f2dfbc32d5
Varezhka69 <vareckakuznecova@gmail.com>
2024-12-12 12:25:10 +0300

    Create Time.py

---
+++ Src/1/Moduls/ImageLoader/Time.py
@@ -0,0 +1,30 @@
+import os
+import time
+from PIL import Image
+import sys
+
+class ImageLoader:
+    def __init__(self, image_path):
+        self.image_path = image_path
+        self.image = None
+        self.load_image()
+
+    def load_image(self):
+        """Загружает изображение и проверяет его наличие."""
+        start_time = time.time()
+
+        if os.path.exists(self.image_path):
+            self.image = Image.open(self.image_path)
+            load_time = time.time() - start_time
+            print(f"Изображение загружено за {load_time:.6f} секунд.")
+        else:
+            raise FileNotFoundError(f"Файл {self.image_path} не найден.")
+
+    def get_image_size(self):
+        """Возвращает размер загруженного изображения в байтах."""
+        if self.image is not None:
+            image_size = sys.getsizeof(self.image)
+            print(f"Размер объекта изображения: {image_size} байт.")
+            return image_size
+        else:
+            raise ValueError("Изображение не загружено.")
